88738afe460e18a138a33b5b3ce308af0173481e
doris <386108425@qq.com>
2019-07-02 23:43:35 +0800

    参考老师代码

--- src/router.js
+++ src/router.js
@@ -3,6 +3,8 @@ import Router from 'vue-router'
 import Login from './views/Login/Login.vue'
 import Home from './views/Home/Home.vue'
 import User from './views/User/User.vue'
+import Roles from './views/Roles/Roles.vue'
+import Rights from './views/Rights/Rights.vue'
 Vue.use(Router)
 
 export default new Router({
@@ -18,12 +20,20 @@ export default new Router({
         {
           path: '/user',
           component: User
+        },
+        {
+          path: '/roles',
+          component: Roles
+        },
+        {
+          path: 'rights',
+          component: Rights
         }
       ]
     },
     {
       path: '/',
-      redirect: '/login'
+      redirect: '/home'
     }
   ]
 })
