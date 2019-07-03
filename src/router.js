7a1a55962eb9656fbd1845805b6f76a2d4774504
doris <386108425@qq.com>
2019-07-03 09:18:37 +0800

    rights权限列表页面信息渲染

--- src/router.js
+++ src/router.js
@@ -26,7 +26,7 @@ export default new Router({
           component: Roles
         },
         {
-          path: 'rights',
+          path: '/rights',
           component: Rights
         }
       ]
